99c7801747aa713471de104723405bb0b714d79f
Cheikh Gaye <cheikhgaye1@esp.sn>
2025-05-20 15:16:25 +0000

    variables in terraform

--- ex3/Instance.tf
+++ ex3/Instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  ami               = data.aws_ami.amiID.id
-  instance_type     = "t2.micro"
+  ami               = var.AMI_ID[var.REGION]
+  instance_type     = var.INST_TYPE
   key_name          = aws_key_pair.dove-key-pair.key_name
   security_groups   = [aws_security_group.dove_sg.name]
-  availability_zone = "us-east-1a"
+  availability_zone = var.ZONE
 
   tags = {
     Name    = "dove-app"
