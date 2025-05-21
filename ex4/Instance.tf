a13479e526a36978bf53bd2219a3e86150140d7f
Cheikh Gaye <cheikhgaye1@esp.sn>
2025-05-21 10:07:13 +0000

    exercice 4

---
+++ ex4/Instance.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "web" {
+  ami               = var.AMI_ID[var.REGION]
+  instance_type     = var.INST_TYPE
+  key_name          = aws_key_pair.dove-key-pair.key_name
+  security_groups   = [aws_security_group.dove_sg.name]
+  availability_zone = var.ZONE
+
+  tags = {
+    Name    = "dove-app"
+    Project = "Dove"
+  }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.USER
+    host        = self.public_ip
+    private_key = file("dove")
+  }
+
+  provisioner "remote-exec" {
+    inline = ["chmod +x /tmp/web.sh", "sudo /tmp/web.sh"]
+  }
+}
+
+resource "aws_ec2_instance_state" "web-state" {
+  instance_id = aws_instance.web.id
+  state       = "running"
+}
+
+output "public_ip" {
+  description = "Instance Public ip"
+  value       = aws_instance.web.public_ip
+}
+
